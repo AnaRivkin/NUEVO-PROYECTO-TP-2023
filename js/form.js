21341a69bc5c34d2f0edab497a1f31d78d6f2868
MarcosGual <marcos.gual@outlook.com>
2023-10-12 13:15:04 -0300

    add: formulario con validaciones + select

---
+++ js/form.js
@@ -0,0 +1,34 @@
+const form = document.getElementById('formOpinion');
+const tipoContacto = document.getElementById('tipoContacto');
+
+
+tipoContacto.addEventListener('change', (e) => {
+    const nombreCampoTexto = document.getElementById('nombreCampoTexto');
+    const campoConsulta = document.getElementById('campoConsulta');
+
+    if (e.target.value === 'consulta') {
+        nombreCampoTexto.textContent = 'Consulta:';
+        campoConsulta.placeholder = 'Escribí tu consulta...';
+    } else if (e.target.value === 'opinion') {
+        nombreCampoTexto.textContent = 'Opinión:';
+        campoConsulta.placeholder = 'Escribí tu opinión...';
+    }
+})
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const nombre = document.getElementById('firstname').value;
+    const apellido = document.getElementById('lastname').value;
+    const email = document.getElementById('email').value;
+    const tipoContactoValue = tipoContacto.value;
+
+    const tipoConsulta = document.querySelector('input[name="consulta"]:checked').value;
+
+    if (nombre && apellido && email && tipoConsulta && tipoContacto) {
+        swal(`Muchas gracias, ${nombre}!`, `Tu ${tipoContactoValue} fue enviada!`, "success");
+    }
+    else {
+        swal(`Faltan datos`, `Faltan completar campos del formulario...`, "error");
+    }
+})
