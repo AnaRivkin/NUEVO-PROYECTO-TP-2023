5214b0b69bad77c4e7aebbfebfe349f945f38dff
MarcosGual <marcos.gual@outlook.com>
2023-10-23 23:23:03 -0300

    fix: api de frases en español agregada

--- js/index.js
+++ js/index.js
@@ -2,12 +2,36 @@ const body = document.body;
 body.onload = agregarCita();
 
 async function agregarCita() {
-    const res = await fetch('https://zenquotes.io/api/today');
-    const [json] = await res.json();
+    const res = await fetch('https://frasedeldia.azurewebsites.net/api/phrase');
+    const json = await res.json();
 
     const cita = document.getElementById('quote');
     const autor = document.getElementById('author');
 
-    cita.textContent = json.q;
-    autor.textContent = json.a;
+    console.log(json)
+
+    // const citaTraducida = await traducirCita(json.q);
+
+    cita.textContent = json.phrase;
+    autor.textContent = json.author;
 };
+
+// async function traducirCita(cita) {
+//     const res = await fetch("https://es.libretranslate.com/translate", {
+//         method: "POST",
+//         body: JSON.stringify({
+//             q: cita,
+//             source: "auto",
+//             target: "es",
+//             format: "text",
+//             api_key: ""
+//         }),
+//         headers: { "Content-Type": "application/json" }
+//     });
+
+//     const translation = await res.json();
+
+//     console.log(translation);
+
+//     return
+// }
